2f968475368c57f1b92cc77a1943184ca0161c92
XHackerzkali <64786928+XHackerzkali@users.noreply.github.com>
2020-07-19 22:06:00 +0530

    Update __main__.py

--- tobrot/__main__.py
+++ tobrot/__main__.py
@@ -134,13 +134,13 @@ if __name__ == "__main__" :
     #
     save_thumb_nail_handler = MessageHandler(
         save_thumb_nail,
-        filters=Filters.command([Commandi.SAVETHUMBNAIL@kalileechbot]) & Filters.chat(chats=AUTH_CHANNEL)
+        filters=Filters.command(["Savethumbnail@KaliLeechBot"]) & Filters.chat(chats=AUTH_CHANNEL)
     )
     app.add_handler(save_thumb_nail_handler)
     #
     clear_thumb_nail_handler = MessageHandler(
         clear_thumb_nail,
-        filters=Filters.command([Commandi.CLEARTHUMBNAIL@kalileechbot]) & Filters.chat(chats=AUTH_CHANNEL)
+        filters=Filters.command(["Clearthumbnail@KaliLeechBot"]) & Filters.chat(chats=AUTH_CHANNEL)
     )
     app.add_handler(clear_thumb_nail_handler)
     # 
